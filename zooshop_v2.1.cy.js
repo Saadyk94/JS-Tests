7785db28a8b2da282628700f2a97a43f2e2239b0
Saadyk <117447098+Saadyk94@users.noreply.github.com>
2023-01-16 13:48:25 +0600

    update

--- zooshop_v2.1.cy.js
+++ zooshop_v2.1.cy.js
@@ -1,9 +1,7 @@
-describe('Проверка раскрывающего меню',  () => {
-    it('ввод в работу раскрывающего меню', () => {
-        cy.visit('https://zooshop.online/');
-    it('проверка кнопки выбора языка', () => {
-        cy.get('[title="Latviešu"] > :nth-child(1) > .wpml-ls-native').click()})
-  
+describe('Проверка коррекности навигации по сайту',  () => {
+    it('ввод в работу раскрывающего меню сайта', () => {
+        cy.visit('https://zooshop.online/');  
+        cy.get('[title="Latviešu"] > :nth-child(1) > .wpml-ls-native').click();
         cy.get('[href="https://zooshop.online/product-category/suniem-metala-buri-matraci-transportesanas-kastes-somas-manezas/"] > span').trigger('mouseover');
         cy.get('#menu-item-6344 > :nth-child(1) > span').trigger('mouseover');
         cy.get('[href="https://zooshop.online/product-category/bariba-kakiem-suniem-grauzejiem-putniem/"] > span').trigger('mouseover');
@@ -11,7 +9,6 @@ describe('Проверка раскрывающего меню',  () => {
         cy.get('[href="https://zooshop.online/product-category/preces-putniem/"] > span').trigger('mouseover');
         cy.get('[href="https://zooshop.online/product-category/preces-grauzejiem/"] > span').trigger('mouseover');
         cy.get('[href="https://zooshop.online/product-category/piederumi-akvarija-zivim/"] > span').trigger('mouseover');
-        cy.get('#menu-item-11064 > a > span').trigger('mouseover');
         cy.get('#menu-item-11064 > a > span').trigger('mouseover');
         cy.get('[href="https://zooshop.online/product-category/bariba-kakiem-suniem-grauzejiem-putniem/"] > span').click();
         cy.get('#menu-item-3609 > [href="https://zooshop.online/product-category/suniem-metala-buri-matraci-transportesanas-kastes-somas-manezas/"] > span').click();
